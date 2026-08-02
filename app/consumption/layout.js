e6205ff1f62778bbdca336189f462977e83a1bda
Hikky-fuji <hikarufujimoto1992@gmail.com>
2026-08-02 14:51:17 -0400

    Improve public readiness and Phillips curve

---
+++ app/consumption/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: { absolute: 'Japan Household Consumption | JapanPulse' },
+  description: 'Japan household spending and consumption dashboard using the Family Income and Expenditure Survey.',
+  alternates: { canonical: '/consumption' },
+}
+
+export default function ConsumptionLayout({ children }) {
+  return children
+}
