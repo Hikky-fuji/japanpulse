9cfc8b9001bcb38a6f519301e629867ea749215b
Hikky-fuji <hikarufujimoto1992@gmail.com>
2026-08-02 14:51:31 -0400

    Improve public readiness and Phillips curve

---
+++ app/tsip/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: { absolute: 'Japan Tertiary Activity | JapanPulse' },
+  description: 'Japan tertiary-industry activity dashboard covering major service sectors using official METI data.',
+  alternates: { canonical: '/tsip' },
+}
+
+export default function TsipLayout({ children }) {
+  return children
+}
