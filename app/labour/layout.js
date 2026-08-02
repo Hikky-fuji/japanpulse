3015cb88f768573af0e559411be1a86d80d416d5
Hikky-fuji <hikarufujimoto1992@gmail.com>
2026-08-02 14:51:22 -0400

    Improve public readiness and Phillips curve

---
+++ app/labour/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: {
+    absolute: 'Japan Labour Market & Phillips Curve | JapanPulse',
+  },
+  description:
+    'Japan unemployment, employment, participation, real wages and Phillips curve dashboard using official MIC and MHLW data.',
+  alternates: {
+    canonical: '/labour',
+  },
+}
+
+export default function LabourLayout({ children }) {
+  return children
+}
