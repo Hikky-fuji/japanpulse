54447846b3053cbd106391587bc6bfcef2e7caa3
Hikky-fuji <hikarufujimoto1992@gmail.com>
2026-08-02 10:45:18 -0400

    Add app/us/consumption/layout.js

---
+++ app/us/consumption/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: {
+    absolute: 'US PCE & Consumer | JapanPulse',
+  },
+  description:
+    'US PCE inflation, real consumption, disposable income, goods and services demand, and personal saving dashboard.',
+}
+
+export default function ConsumptionLayout({ children }) {
+  return children
+}
