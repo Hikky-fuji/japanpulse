a405842a90b3b0af45818d6ab09f9c95a380ae5e
Hikky-fuji <hikarufujimoto1992@gmail.com>
2026-08-02 10:45:24 -0400

    Add app/us/jolts/layout.js

---
+++ app/us/jolts/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: {
+    absolute: 'US JOLTS | JapanPulse',
+  },
+  description:
+    'US job openings, hires, quits, layoffs, unemployment and labor-market tightness dashboard.',
+}
+
+export default function JoltsLayout({ children }) {
+  return children
+}
